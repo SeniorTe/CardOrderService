4f40d22b739a9517845800fa6bf915428330904e
Menshikov.SI <89089118626@mail.ru>
2023-03-07 15:59:13 +0500

    create yml and first test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Домашнее задание к занятию «2.1. Тестирование веб-интерфейсов»
 
+[![Build status](https://ci.appveyor.com/api/projects/status/1g4nr8g6qes69fr5?svg=true)](https://ci.appveyor.com/project/SeniorTe/cardorderservice)
---
+++ src/test/java/ru/netology/CardOrderServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class CardOrderServiceTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void test() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Сергей");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998888888");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__content")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+
+}
